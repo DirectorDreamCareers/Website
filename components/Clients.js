bc85c6e8f66d4fb6de8a58d7b1ba60963d7c62fb
Alvvion <alvvion@gmail.com>
2022-05-28 12:50:47 +0530

    Commit-2

---
+++ components/Clients.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Clients() {
+  return (
+    <div className='py-24' style={{backgroundColor: '#e3f6f9'}}>
+    <div className='px-4 mx-auto xl:max-w-6xl lg:max-w-4xl md:max-w-2xl'>
+      <div className='relative px-4'>
+        <div className='mb-20 text-center'>
+          <h1 className='text-6xl font-semibold' style={{fontFamily: 'sans-serif', color: '#0a325a'}}>Our <span style={{color: '#f2a24f'}}>Clients</span></h1>
+        </div>
+      </div>
+     
+    </div>
+  </div>
+  )
+}
+
+export default Clients
---
+++ components/Navbar.js
@@ -0,0 +1,131 @@
+import React from 'react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import {Fragment} from 'react'
+import { Popover, Transition } from '@headlessui/react'
+
+function Navbar() {
+    const navigation = [
+        { name: "About Us", href: "#" },
+        { name: "Services", href: "#" },
+        { name: "Marketplace", href: "#" },
+        { name: "Company", href: "#" },
+    ];
+    return (
+        <Popover as="header" className="relative">
+          <div className="relative">
+            <nav
+              className="flex items-center h-auto px-4 py-2 mx-auto bg-gray-900 xl:left-0 xl:right-0 xl:fixed max-w-7xl sm:px-6 xl:top-1 z-20 rounded-md"
+              aria-label="Global"
+            >
+              <div className="flex items-center flex-1">
+                <div className="flex items-center justify-between w-full md:w-auto">
+                  <a href="#">
+                    <span className="sr-only">Workflow</span>
+                    <img
+                      className="w-auto h-8 sm:h-10"
+                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                      alt=""
+                    />
+                  </a>
+                  <div className="flex items-center -mr-2 md:hidden">
+                    <Popover.Button className="inline-flex items-center justify-center p-2 text-gray-400 bg-gray-900 rounded-md hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
+                      <span className="sr-only">Open main menu</span>
+                      <MenuIcon className="w-6 h-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="hidden space-x-8 md:flex md:ml-10">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="text-base font-medium text-white hover:text-gray-300"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+              <div className="hidden md:flex md:items-center md:space-x-6">
+                <a
+                  href="#"
+                  className="text-base font-medium text-white hover:text-gray-300"
+                >
+                  Log in
+                </a>
+                <a
+                  href="#"
+                  className="inline-flex items-center px-4 py-2 text-base font-medium text-white bg-gray-600 border border-transparent rounded-md hover:bg-gray-700"
+                >
+                  Start free trial
+                </a>
+              </div>
+            </nav>
+          </div>
+
+          <Transition
+            as={Fragment}
+            enter="duration-150 ease-out"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="duration-100 ease-in"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Popover.Panel
+              focus
+              className="absolute inset-x-0 top-0 z-10 p-2 transition origin-top transform md:hidden"
+            >
+              <div className="overflow-hidden bg-white rounded-lg shadow-md ring-1 ring-black ring-opacity-5">
+                <div className="flex items-center justify-between px-5 pt-4">
+                  <div>
+                    <img
+                      className="w-auto h-8"
+                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="-mr-2">
+                    <Popover.Button className="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600">
+                      <span className="sr-only">Close menu</span>
+                      <XIcon className="w-6 h-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="pt-5 pb-6">
+                  <div className="px-2 space-y-1">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="block px-3 py-2 text-base font-medium text-gray-900 rounded-md hover:bg-gray-50"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                  <div className="px-5 mt-6">
+                    <a
+                      href="#"
+                      className="block w-full px-4 py-3 font-medium text-center text-white bg-indigo-600 rounded-md shadow hover:bg-indigo-700"
+                    >
+                      Start free trial
+                    </a>
+                  </div>
+                  <div className="px-5 mt-6">
+                    <p className="text-base font-medium text-center text-gray-500">
+                      Existing customer?{" "}
+                      <a href="#" className="text-gray-900 hover:underline">
+                        Login
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Popover.Panel>
+          </Transition>
+        </Popover>
+    )
+}
+
+export default Navbar
